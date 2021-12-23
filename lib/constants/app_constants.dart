b0686e6406f1a5da9c5dc65eb86d779d46919d2c
khduy <duyhk1223@gmail.comm>
2021-12-23 23:11:19 +0700

    Update img onboard screen

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -20,6 +20,12 @@ const libraryTitle = [
   'Recent activity',
 ];
 
+const onboardImg = [
+  'https://firebasestorage.googleapis.com/v0/b/mixturemusic-f0b97.appspot.com/o/onboardImage%2Fab67616d0000b2731ae71e77aef7c34227027daa.jpg?alt=media&token=d6efa888-b8ac-4bc9-93d3-69dcfa6078b5',
+  'https://firebasestorage.googleapis.com/v0/b/mixturemusic-f0b97.appspot.com/o/onboardImage%2FDark%20Paradise%20-%20Lana%20Del%20Rey.jpeg?alt=media&token=88a964b9-378f-4a49-810f-5ce6016f926a',
+  'https://firebasestorage.googleapis.com/v0/b/mixturemusic-f0b97.appspot.com/o/onboardImage%2FDowntown%20-%20Allie%20X.jpeg?alt=media&token=349578a0-e0a6-44ce-a09e-ddf291d1054c',
+  'https://firebasestorage.googleapis.com/v0/b/mixturemusic-f0b97.appspot.com/o/onboardImage%2FDean%20Lewis%20-%20Be%20Alright.jpeg?alt=media&token=d2ec7e56-58f2-47ca-a258-c113336f395d',
+];
 
 const accountScreenGridIcon = [
   Icons.library_music,
@@ -43,12 +49,14 @@ final artistModels = <ArtistModel>[
   ArtistModel(
     artistId: '0',
     artistName: 'Erik',
-    imageUrl: 'https://media-cdn.laodong.vn/Storage/NewsPortal/2021/8/3/937782/118654438_7452735827.jpg',
+    imageUrl:
+        'https://media-cdn.laodong.vn/Storage/NewsPortal/2021/8/3/937782/118654438_7452735827.jpg',
   ),
   ArtistModel(
     artistId: '1',
     artistName: 'Đức Phúc',
-    imageUrl: 'https://image.thanhnien.vn/768/uploaded/hienth/2020_01_23/53167553_2122687297838771_6964634450827149312_o_gnbk.jpg',
+    imageUrl:
+        'https://image.thanhnien.vn/768/uploaded/hienth/2020_01_23/53167553_2122687297838771_6964634450827149312_o_gnbk.jpg',
   ),
   ArtistModel(
     artistId: '2',
@@ -58,7 +66,8 @@ final artistModels = <ArtistModel>[
   ArtistModel(
     artistId: '3',
     artistName: 'Đen Vâu',
-    imageUrl: 'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
+    imageUrl:
+        'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
   ),
   ArtistModel(
     artistId: '4',
@@ -67,15 +76,16 @@ final artistModels = <ArtistModel>[
   ),
 ];
 
-
 final listSong = [
   SongModel(
     id: 'id',
     data: SongData(
       title: 'Trời hôm nay nhiều mây cực cực cực cực cực cực',
       artist: 'Đen Vâu',
-      imgURL: 'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
-      audioURL: 'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
+      imgURL:
+          'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
+      audioURL:
+          'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
     ),
   ),
   SongModel(
@@ -84,7 +94,8 @@ final listSong = [
       title: 'Vết mưa',
       artist: 'Vũ Cát Tường',
       imgURL: 'https://avatar-ex-swe.nixcdn.com/playlist/2013/10/29/e/d/1/2/1383055637469_500.jpg',
-      audioURL: 'https://avatar-ex-swe.nixcdn.com/playlist/2013/10/29/e/d/1/2/1383055637469_500.jpg',
+      audioURL:
+          'https://avatar-ex-swe.nixcdn.com/playlist/2013/10/29/e/d/1/2/1383055637469_500.jpg',
     ),
   ),
   SongModel(
@@ -101,8 +112,10 @@ final listSong = [
     data: SongData(
       title: 'Gọi mưa',
       artist: 'Trung Quân Idol',
-      imgURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
-      audioURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
+      imgURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
+      audioURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
     ),
   ),
   SongModel(
@@ -110,8 +123,10 @@ final listSong = [
     data: SongData(
       title: 'Trời hôm nay nhiều mây cực',
       artist: 'Đen Vâu',
-      imgURL: 'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
-      audioURL: 'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
+      imgURL:
+          'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
+      audioURL:
+          'https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/9/0/2/2/90223f08b220e52a78ac5c0dd739256f.jpg',
     ),
   ),
   SongModel(
@@ -119,8 +134,10 @@ final listSong = [
     data: SongData(
       title: 'Dấu mưa',
       artist: 'Trung Quân Idol',
-      imgURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
-      audioURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
+      imgURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
+      audioURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
     ),
   ),
   SongModel(
@@ -138,7 +155,8 @@ final listSong = [
       title: 'Vết mưa',
       artist: 'Vũ Cát Tường',
       imgURL: 'https://avatar-ex-swe.nixcdn.com/playlist/2013/10/29/e/d/1/2/1383055637469_500.jpg',
-      audioURL: 'https://avatar-ex-swe.nixcdn.com/playlist/2013/10/29/e/d/1/2/1383055637469_500.jpg',
+      audioURL:
+          'https://avatar-ex-swe.nixcdn.com/playlist/2013/10/29/e/d/1/2/1383055637469_500.jpg',
     ),
   ),
   SongModel(
@@ -146,8 +164,10 @@ final listSong = [
     data: SongData(
       title: 'Gọi mưa',
       artist: 'Trung Quân Idol',
-      imgURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
-      audioURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
+      imgURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
+      audioURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/0/0/003a856bfc8a2109ca5b5603e2ef5f75_1406285052.jpg',
     ),
   ),
   SongModel(
@@ -155,8 +175,10 @@ final listSong = [
     data: SongData(
       title: 'Dấu mưa',
       artist: 'Trung Quân Idol',
-      imgURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
-      audioURL: 'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
+      imgURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
+      audioURL:
+          'https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/5/f/5f732a84bfba6ba0230e11ef4e49ba38_1392691168.jpg',
     ),
   ),
 ];
