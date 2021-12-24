32b98c3eafe3430d6fc1e140086ea2bb9e251b5b
khduy <duyhk1223@gmail.comm>
2021-12-25 00:11:15 +0700

    Remove unuse code, add empty widget favorite

--- lib/ui/library/views/favourite_view.dart
+++ lib/ui/library/views/favourite_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mixture_music_app/constants/enums/enums.dart';
 import 'package:mixture_music_app/controllers/user_data_controller.dart';
+import 'package:mixture_music_app/images/app_images.dart';
 import 'package:mixture_music_app/ui/library/widgets/library_grid_view_card.dart';
 import 'package:mixture_music_app/ui/library/widgets/shuffle_and_swap_view.dart';
 import 'package:mixture_music_app/ui/player_screen/controller/music_player_controller.dart';
@@ -59,43 +60,64 @@ class _LibraryListViewState extends State<_LibraryListView> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: Obx(
-        () => ListView.separated(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: _userDataController.favorites.length,
-          itemBuilder: (context, index) {
-            return Obx(
-              () => SongTile(
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 32,
+    return Obx(
+      () => Align(
+        alignment: Alignment.topCenter,
+        child: _userDataController.favorites.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                    Image.asset(
+                      AppImages.playlist,
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: MediaQuery.of(context).size.width * 0.3,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 16.0),
+                    Text(
+                      'You have no favorites',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          ?.copyWith(fontSize: 18.0, fontWeight: FontWeight.w400),
+                    ),
+                  ],
                 ),
-                songModel: _userDataController.favorites[index],
-                isPlaying: _musicController.playingSong.value != null
-                    ? _musicController.playingSong.value!.id ==
-                            _userDataController.favorites[index].id
-                        ? true
-                        : false
-                    : false,
-                onTap: () async {
-                  _musicController.setSong(
-                    _userDataController.favorites[index],
+              )
+            : ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: _userDataController.favorites.length,
+                itemBuilder: (context, index) {
+                  return SongTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 32,
+                    ),
+                    songModel: _userDataController.favorites[index],
+                    isPlaying: _musicController.playingSong.value != null
+                        ? _musicController.playingSong.value!.id ==
+                                _userDataController.favorites[index].id
+                            ? true
+                            : false
+                        : false,
+                    onTap: () async {
+                      _musicController.setSong(
+                        _userDataController.favorites[index],
+                      );
+                    },
+                    isFavorite: _userDataController.favorites.contains(
+                      _userDataController.favorites[index],
+                    ),
                   );
                 },
-                isFavorite: _userDataController.favorites.contains(
-                  _userDataController.favorites[index],
+                separatorBuilder: (context, index) => const Divider(
+                  height: 1,
+                  thickness: 0.5,
                 ),
               ),
-            );
-          },
-          separatorBuilder: (context, index) => const Divider(
-            height: 1,
-            thickness: 0.5,
-          ),
-        ),
       ),
     );
   }
@@ -114,41 +136,64 @@ class _LibraryGridViewState extends State<_LibraryGridView> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Obx(
-            () => Wrap(
-              spacing: 24.0,
-              runSpacing: 8.0,
-              children: [
-                ...List.generate(
-                  _userDataController.favorites.length,
-                  (index) => Container(
-                    margin: const EdgeInsets.only(top: 16.0),
-                    child: LibraryGridViewCard(
-                      songModel: _userDataController.favorites[index],
-                      onTap: () {
-                        _musicController.setSong(
-                          _userDataController.favorites[index],
-                        );
-                      },
-                      imageRadius: BorderRadius.circular(16.0),
-                      isPlaying: _musicController.playingSong.value != null
-                          ? _musicController.playingSong.value!.id ==
-                                  _userDataController.favorites[index].id
-                              ? true
-                              : false
-                          : false,
+    return Obx(
+      () => Align(
+        alignment: Alignment.topCenter,
+        child: _userDataController.favorites.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                    Image.asset(
+                      AppImages.playlist,
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: MediaQuery.of(context).size.width * 0.3,
+                      fit: BoxFit.cover,
                     ),
-                  ),
+                    const SizedBox(height: 16.0),
+                    Text(
+                      'You have no favorites',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          ?.copyWith(fontSize: 18.0, fontWeight: FontWeight.w400),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Wrap(
+                    spacing: 24.0,
+                    runSpacing: 8.0,
+                    children: [
+                      ...List.generate(
+                        _userDataController.favorites.length,
+                        (index) => Container(
+                          margin: const EdgeInsets.only(top: 16.0),
+                          child: LibraryGridViewCard(
+                            songModel: _userDataController.favorites[index],
+                            onTap: () {
+                              _musicController.setSong(
+                                _userDataController.favorites[index],
+                              );
+                            },
+                            imageRadius: BorderRadius.circular(16.0),
+                            isPlaying: _musicController.playingSong.value != null
+                                ? _musicController.playingSong.value!.id ==
+                                        _userDataController.favorites[index].id
+                                    ? true
+                                    : false
+                                : false,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
       ),
     );
   }
